6b7253593343dc3afcf6d0c12fdc7085a97df132
Nicolas Barra <nicolasbarrapelecano@gmail.com>
2022-08-23 20:22:12 -0300

    modelado de datos

--- organizador/contact/models.py
+++ organizador/contact/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+class Contact (models.Model):
+    name = models.CharField(max_length=50, blank=False, null=False)
+    last_name = models.CharField(max_length=50, blank=True, null=True)
+    email = models.EmailField(max_length=100)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    mobile = models.CharField(max_length=20, blank=False, null=False)
+    company = models.CharField(max_length=50, blank=True, null=True)
+    date = models.DateField(auto_now_add=True)
+    notes = models.TextField(blank=True, null=True)
+    
+
+    def __str__(self):
+        return self.name 
 
--- organizador/todo/models.py
+++ organizador/todo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=100, blank=False, null=False)
+    description = models.TextField(blank=True, null=True)
+    completed = models.BooleanField(default=False)
+    priority = models.IntegerField(default = 3)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
