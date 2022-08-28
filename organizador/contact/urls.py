7ff679a819ec180073e115d5aa1fc82cf7d5328d
Nicolas Barra <nicolasbarrapelecano@gmail.com>
2022-08-28 20:18:28 -0300

    delete contact

--- organizador/contact/urls.py
+++ organizador/contact/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
    path('view/<int:id>', views.view, name='contact_view'),
    path('edit/<int:id>', views.edit, name='contact_edit'),
    path('create/', views.create, name='contact_create'),
-   #path('delete/<int:id>', views.delete, name='contact_delete'),
+   path('delete/<int:id>', views.delete, name='contact_delete'),
 ]
--- organizador/contact/views.py
+++ organizador/contact/views.py
@@ -56,3 +56,8 @@ def create (request):
         if form.is_valid():
             form.save()
         return redirect('contact')
+
+def delete(request, id):
+    contact = Contact.objects.get(id=id)
+    contact.delete()
+    return redirect('contact')
